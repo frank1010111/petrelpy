1eff95f2631fdac782993b1875c1ddab4a4a430b
frank1010111 <frmale@utexas.edu>
2023-04-22 00:45:01 -0400

    :test_tube: test cli command for production conversion

--- tests/test_petrel.py
+++ tests/test_petrel.py
@@ -1,7 +1,30 @@
 from __future__ import annotations
 
-from petrelpy import petrel as p
+from pathlib import Path
+
+import pytest
+from click.testing import CliRunner
+from petrelpy.cli import cli
 
 
-def test_import():
-    p.__name__
+@pytest.mark.parametrize("yearly", [True, False])
+def test_cli_production(yearly):
+    runner = CliRunner()
+    input_csv = (
+        Path(__file__).parent
+        / f"data/test_{'yearly' if yearly else 'monthly'}_prod.csv"
+    )
+    with runner.isolated_filesystem() as td:
+        out_file = Path(td) / "test_monthly_prod.vol"
+        args = ["production", f"{input_csv}", "-o", f"{out_file}"]
+        if yearly:
+            args += ["-y"]
+            assert args[-1] == "-y"
+        result = runner.invoke(cli, args)
+        test_output = out_file.open().read()
+    assert result.exit_code == 0
+    output_vol = Path("tests/data/test_monthly_prod.vol").open().read()
+    if yearly:
+        assert test_output == output_vol.replace("*MONTHLY", "*YEARLY")
+    else:
+        assert test_output == output_vol
