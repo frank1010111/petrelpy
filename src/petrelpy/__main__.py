fac7fb0e8fed0a090a66ee455289badab9c268da
Frank Male <frmale@utexas.edu>
2023-05-08 21:16:52 -0400

    :hammer::sparkles: add __main__.py, ease callability

---
+++ src/petrelpy/__main__.py
@@ -0,0 +1,9 @@
+"""Command line tool for working with Petrel input and output formats."""
+from __future__ import annotations
+
+import sys
+
+if __name__ == "__main__":
+    from petrelpy.cli import cli
+
+    sys.exit(cli())
