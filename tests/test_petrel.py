3e1f42a3b7543c55f02af98e8ec6eb0f489ce089
Frank Male <frmale@utexas.edu>
2023-05-08 21:24:26 -0400

    :test_tube: tests to process well connection files

--- tests/test_petrel.py
+++ tests/test_petrel.py
@@ -1,3 +1,4 @@
+"""Test petrelpy functions."""
 from __future__ import annotations
 
 from pathlib import Path
@@ -28,3 +29,24 @@ def test_cli_production(yearly):
         assert test_output == output_vol.replace("*MONTHLY", "*YEARLY")
     else:
         assert test_output == output_vol
+
+
+def test_cli_wellconnection():
+    runner = CliRunner()
+    with runner.isolated_filesystem() as td:
+        wcf_file = Path(__file__).parent / "data/test_wcf.wcf"
+        well_heel_file = Path(__file__).parent / "data/test_heels.csv"
+        out_file = Path(td) / "test_properties.csv"
+        args = [
+            "connection",
+            f"{wcf_file}",
+            "--heel",
+            f"{well_heel_file}",
+            "-o",
+            f"{out_file}",
+        ]
+        result = runner.invoke(cli, args)
+        assert result.exit_code == 0
+        bench = wcf_file.with_suffix(".csv")
+        with Path(out_file).open() as f_output, Path(bench).open() as f_benchmark:
+            assert f_output.read() == f_benchmark.read()
