d8d99af07be4e4a50b7d29e24f1e054cda1cbe23
Frank Male <frmale@utexas.edu>
2023-05-13 14:17:59 -0400

    :test_tube: test glib CLI command

--- tests/test_petrel.py
+++ tests/test_petrel.py
@@ -80,3 +80,37 @@ def test_cli_perforations(input_format, output_format):
             bench = main_perf_file.parent / "benchmark_perf.prn"
         with output_file.open() as f_output, bench.open() as f_benchmark:
             assert f_output.read() == f_benchmark.read()
+
+
+@pytest.mark.parametrize("output_format", ["csv", "parquet"])
+def test_cli_gslib(output_format):
+    runner = CliRunner()
+    with runner.isolated_filesystem() as td:
+        gslib_file = Path(__file__).parent / "data/test_geomodel.gslib"
+        output_file = Path(td) / "geomodel"
+        args = [
+            "gslib",
+            f"{gslib_file}",
+            "-o",
+            f"{output_file}",
+            "--output_format",
+            f"{output_format}",
+        ]
+        result = runner.invoke(cli, args)
+        assert result.exit_code == 0
+        if output_format == "csv":
+            bench = gslib_file.with_suffix(".csv")
+            with output_file.with_suffix(
+                ".csv"
+            ).open() as f_output, bench.open() as f_benchmark:
+                assert f_output.read() == f_benchmark.read()
+        else:  # output_format == "parquet"
+            benchmark = pd.read_csv(gslib_file.with_suffix(".csv")).sort_values(
+                ["i_index", "j_index"]
+            )
+            result_frame = pd.read_parquet(
+                output_file.with_suffix(".parquet")
+            ).sort_values(["i_index", "j_index"])
+            assert set(benchmark.columns) == set(result_frame.columns)
+            for col in benchmark.columns:
+                assert pytest.approx(benchmark[col]) == result_frame[col]
