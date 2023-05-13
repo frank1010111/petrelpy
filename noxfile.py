ffe93411556b2d52e9ec9eb5b22ee2fdefc111ff
Frank Male <frmale@utexas.edu>
2023-05-13 00:47:37 -0400

    :hook: nox docs session complains unless sphinx-build is tagged external

--- noxfile.py
+++ noxfile.py
@@ -33,7 +33,7 @@ def docs(session: nox.Session) -> None:
     """Build the docs. Pass "serve" to serve."""
     session.install(".[docs]")
     session.chdir("docs")
-    session.run("sphinx-build", "-M", "html", ".", "_build")
+    session.run("sphinx-build", "-M", "html", ".", "_build", external=True)
 
     if session.posargs:
         if "serve" in session.posargs:
