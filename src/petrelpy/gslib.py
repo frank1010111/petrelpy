72b9054c912d08db4afc7e5b13933e7a9820cf14
Frank Male <frmale@utexas.edu>
2023-05-13 13:26:22 -0400

    :recycle: refactor gslib utilities

--- src/petrelpy/gslib.py
+++ src/petrelpy/gslib.py
@@ -1,55 +1,56 @@
 """Work with gslib geomodel format."""
 from __future__ import annotations
 
+import logging
+from pathlib import Path
+
 import dask.dataframe as dd
 import fastparquet
 import numpy as np
 import pandas as pd
 from scipy.spatial import cKDTree
 
-long_layerdict = {
-    1: "above",
-    2: "Upper Spraberry",
-    3: "Middle Spraberry",
-    4: "Lower Spraberry",
-    5: "Middle Leonard",
-    6: "Dean",
-    7: "Wolfcamp A",
-    8: "Wolfcamp B",
-    9: "Wolfcamp C1",
-    10: "Wolfcamp C2",
-    11: "Wolfcamp D",
-    12: "Strawn",
-}
 
+def load_from_petrel(fin: Path | str, npartitions=60) -> dd.DataFrame:
+    """Load GSLIB geomodel file.
 
-def load_from_petrel(fin, npartitions=60):
+    Args:
+        fin (Path | str): gslib Petrel output
+        npartitions (int, optional): number of partitions for dask dataframe. Defaults to 60.
+
+    Returns:
+        dd.DataFrame: lazy-evaluated dataframe with geomodel properties
+    """
     numprops = pd.read_csv(fin, sep=" ", skiprows=1, nrows=1, header=None)[0][0]
-
     head = pd.read_csv(fin, sep=" ", skiprows=2, nrows=numprops, header=None)
-
-    df = dd.read_csv(
+    geomodel = dd.read_csv(
         fin,
         sep=" ",
         header=numprops + 1,
         na_values=-999,
         names=[*list(head[0]), "null"],
     )
-    df = df.repartition(npartitions=npartitions).drop("null", 1)
-    return df
+    geomodel = geomodel.repartition(npartitions=npartitions).drop("null", 1)
+    return geomodel
 
 
-def get_midpoint_cell_columns(df, dir_out):
-    """Find cell columns where UWI-index exists
-    df is the ini.
+def get_midpoint_cell_columns(geomodel: dd.DataFrame, dir_out: str):
+    """Find cell columns where UWI-index exists in the geomodel.
+
+    Args:
+        geomodel (dd.DataFrame): geocellular model extracted from gslib file
+        dir_out (str): folder to store the parquet file in
+
+    Returns:
+        pd.DataFrame: vertical column of cells around the well's midpoint
     """
     # ID midpoints
-    df_midpoints = df.dropna(subset=["UWI-index"]).compute()
+    df_midpoints = geomodel.dropna(subset=["UWI-index"]).compute()
 
     # get cells where i or j match the midpoint i and j indexes
-    df_i_or_j = df[
-        (df["i_index"].isin(df_midpoints["i_index"]))
-        & df["j_index"].isin(df_midpoints["j_index"])
+    df_i_or_j = geomodel[
+        (geomodel["i_index"].isin(df_midpoints["i_index"]))
+        & geomodel["j_index"].isin(df_midpoints["j_index"])
     ]
     df_i_or_j.to_parquet(dir_out)
 
@@ -68,20 +69,35 @@ def get_midpoint_cell_columns(df, dir_out):
                 idx[df_midpoints["i_index"], df_midpoints["j_index"], :],
             ]
         )
-        print(n, "row groups down")
+        logging.info(n, "row groups down")
         n += 1
     df_ij = pd.concat(df_ij)
     return df_ij
 
 
-def limit_column_height(df_midpoints, df_ij, zdmax=1000):
-    """Cut down on size of model by limiting cell midpoint vertical space to within zdmax of distance
+def _limit_column_height(
+    midpoints: pd.DataFrame, geomodel: dd.DataFrame, zdmax: float = 1000
+):
+    """Cut down on size of model to depths near the midpoint.
+
+    This limits the geomodel cell midpoint vertical space to within zdmax of distance
     between wells (in df_midpoints) and geomodel cells (in df_ij).
+
+    Args:
+        midpoints (pd.DataFrame): well midpoint information,
+            includes the columns i_index,j_index,z_coord
+        geomodel (dd.DataFrame): geocellular model after being limited to vertical columns
+            containing a midpoint
+        zdmax (float, optional): maximum distance between midpoint and cell center.
+            Defaults to 1000.
+
+    Returns:
+        pd.DataFrame: geomodel filtered to cells near well midpoints
     """
     df_out = []
 
-    for _, (i, j, z) in df_midpoints[["i_index", "j_index", "z_coord"]].iterrows():
-        ijmatch = df_ij.xs((i, j), level=(0, 1), drop_level=False)
+    for _, (i, j, z) in midpoints[["i_index", "j_index", "z_coord"]].iterrows():
+        ijmatch = geomodel.xs((i, j), level=(0, 1), drop_level=False)
         # ijmatch = df_ij.xs(i,level=0,drop_level=False).xs(j,level=1,drop_level=False)
 
         df_out.append(ijmatch[abs(ijmatch["z_coord"] - z) < zdmax])
@@ -90,108 +106,139 @@ def limit_column_height(df_midpoints, df_ij, zdmax=1000):
     return df_out
 
 
-def get_header(fin):
-    with open(fin) as f:
-        h = []
-        i = False
+def _get_header(fin: Path | str) -> list[str]:
+    """Extract gslib header."""
+    with Path(fin).open() as f:
+        header_lines = []
+        in_header = False
         for line in f:
             line = line.strip()
             if line == "BEGIN HEADER":
-                i = True
+                in_header = True
+            elif in_header:
+                header_lines.append(line)
             elif line == "END HEADER":
                 break
-            elif i:
-                h.append(line)
-            else:
-                # print(line)
-                continue
-    return h
+    return header_lines
 
 
-def load_petrel_tops_file(fin):
-    "Load in petrel tops file and return pandas dataframe."
-    colnames = get_header(fin)
-    with open(fin) as f:
-        for i, line in enumerate(f):
-            line = line.strip()
-            if line == "END HEADER":
+def load_petrel_tops_file(tops_file: Path | str):
+    """Load in petrel tops file and return pandas dataframe."""
+    colnames = _get_header(tops_file)
+    header_rows = 1
+    with Path(tops_file).open() as f:
+        for line in f:
+            header_rows += 1
+            if line.strip() == "END HEADER":
                 break
-    df = pd.read_csv(fin, skiprows=i + 1, names=colnames, sep="\\s+", na_values=-999)
-    return df
+    well_tops = pd.read_csv(
+        tops_file, skiprows=header_rows, names=colnames, sep="\\s+", na_values=-999
+    )
+    return well_tops
 
 
-def match_well_to_cell(df_wells, df_cells, distance_upper_bound=2000):
-    """Give each well in df_wells with an XYZ position the i,j,k index of the nearest cell in df_cells
-    distance_upper_bound: limit of the max distance between the well and its cell.
+def match_well_to_cell(
+    midpoints: pd.DataFrame, geomodel: pd.DataFrame, distance_upper_bound: float = 2000
+) -> pd.DataFrame:
+    """Give each well in df_wells the i,j,k index of the nearest cell in df_cells.
+
+    Args:
+        midpoints (pd.DataFrame): well midpoint locations. Columns include Well,X,Y,Z
+        geomodel (pd.DataFrame): geocellular model. Columns include x_coord,y_coord,z_coord
+        distance_upper_bound (float, optional): max distance between well and center of cell.
+            Defaults to 2000.
+
+    Returns:
+        pd.DataFrame: geomodel cells nearest to each well midpoint
     """
     # make tree in 3D for getting nearest neighbors
-    tree_cells = cKDTree(df_cells[["x_coord", "y_coord", "z_coord"]])
+    tree_cells = cKDTree(geomodel[["x_coord", "y_coord", "z_coord"]])
     dist, locs = tree_cells.query(
-        well_xyz.loc[:, ["X", "Y", "Z"]],
+        midpoints.loc[:, ["X", "Y", "Z"]],
         k=1,
         distance_upper_bound=distance_upper_bound,
         n_jobs=4,
     )
-    print((~np.isfinite(dist)).sum(), "wells could not get matches")
+    logging.warning((~np.isfinite(dist)).sum(), "wells could not get matches")
 
     # make output dataframe
-    well_cell = df_wells.loc[:, ["Well", "X", "Y", "Z"]]
+    well_cell = midpoints.loc[:, ["Well", "X", "Y", "Z"]]
     for i in ("i_index", "j_index", "k_index"):
         well_cell.loc[:, i] = np.nan
 
     # assign i,j,k for each well to nearest cell
-    for l, _w in well_cell.iterrows():
-        if np.isfinite(dist[l]):
-            well_cell.loc[l, ["i_index", "j_index", "k_index"]] = df_cells.iloc[
-                locs[l]
+    for label, _w in well_cell.iterrows():
+        if np.isfinite(dist[label]):
+            well_cell.loc[label, ["i_index", "j_index", "k_index"]] = geomodel.iloc[
+                locs[label]
             ].name
     return well_cell
 
 
-def match_ijz_petrel(df_full, UWI_toindex, wdir, zdmax=1000):
-    """Puts together above functions to go from two dataframes to a merged dataframe
-    inputs:
-       df_full (dask.DataFrame): dask dataframe of the geomodel, must include a column called 'UWI-index'
-                                 and ijk indices, and z coordinate
-       UWI_toindex (pd.DataFrame): pandas dataframe with UWI-Index column and UWI column to do merging
-       wdir (str): string pointing to the working directory for behind-the-scenes parquet file creation
-       zdmax (float): maximum z variation from cells in ij column to point including a UWI-index
-    outputs:
-       merged (pd.DataFrame): merged dataframe that has all your favorite attributes in an ij column with
-                              less than zdmax vertical separation from midpoint at UWI-index.
+def match_ijz_petrel(
+    geomodel: dd.DataFrame, uwi_to_index: pd.DataFrame, wdir: str, zdmax: float = 1000
+):
+    """Merge geomodel with wells to assign properties to wells.
 
+    Args:
+        geomodel (dask.DataFrame): dask dataframe of the geomodel, must include a column
+            called 'UWI-index', ijk indices, and z coordinate
+        uwi_to_index (pd.DataFrame): pandas dataframe with UWI-Index column and UWI column to do
+            merging
+        wdir (str): string pointing to the working directory for behind-the-scenes parquet file
+            creation
+        zdmax (float): maximum z variation from cells in ij column to point including a UWI-index
+    Returns:
+        pd.DataFrame: merged dataframe that has all your favorite attributes in an ij column with
+        less than zdmax vertical separation from midpoint at UWI-index.
     """
-    df_midpoints = df_full.dropna(subset=["UWI-index"]).compute()
-    df_ijmatched = gs.get_midpoint_cell_columns(df_full, wdir)
-    df_ijmatched_zlim = gs.limit_column_height(df_midpoints, df_ijmatched, zdmax)
-    UWI_to_index["UWI-Index"] = UWI_to_index["UWI-Index"].astype(int)
+    df_midpoints = geomodel.dropna(subset=["UWI-index"]).compute()
+    df_ijmatched = get_midpoint_cell_columns(geomodel, wdir)
+    df_ijmatched_zlim = _limit_column_height(df_midpoints, df_ijmatched, zdmax)
+    uwi_to_index["UWI-Index"] = uwi_to_index["UWI-Index"].astype(int)
     merged = (
         df_ijmatched_zlim.rename(columns={"UWI-index": "UWI-Index"})
         .reset_index()
-        .merge(UWI_to_index, on="UWI-Index", how="left")
+        .merge(uwi_to_index, on="UWI-Index", how="left")
     )
     merged = merged[~merged.index.duplicated(keep="first")]
     return merged
 
 
-def agg_ijz(
-    df_ijmatched, df_midpoints=None, zdmax=10, agg_arg="mean", uwi_col="UWI-Index"
-):
-    # df_wells = gs.agg_ijz(df_ijmatched_zlim,df_midpoints,agg_dict,'UWI-index')
-    if df_midpoints is None:
-        df_midpoints = df_ijmatched.dropna(subset=uwi_col)
-    df_out_index = df_ijmatched[uwi_col].dropna().unique()
-    df_out_columns = df_ijmatched.iloc[0:1].agg(agg_arg).columns
-    df_out = pd.DataFrame(index=df_out_index, columns=df_out_columns)
+def aggregate_well_properties(
+    geomodel_ijmatched: pd.DataFrame,
+    well_midponts: pd.DataFrame | None = None,
+    zdmax: float = 200,
+    agg_arg="mean",
+    uwi_col="UWI-Index",
+) -> pd.DataFrame:
+    """Find average geocellular properties about well midpoint.
 
-    for _, (i, j, z, uwi) in df_midpoints[
+    Args:
+        geomodel_ijmatched (pd.DataFrame): geomodel after paring down to nearby cells
+        well_midponts (pd.DataFrame | None, optional): geocells nearest each well midpoint.
+            Defaults to None. If none, cuts geomodel_ijmatched down to those with a valid uwi_col
+
+        zdmax (float, optional): Max vertical distance between well and geocell. Defaults to 200.
+        agg_arg (str, optional): method for aggregating. Defaults to "mean".
+        uwi_col (str, optional): geocellular column with well identifier. Defaults to "UWI-Index".
+
+    Returns:
+        pd.DataFrame: average properties for each well index
+    """
+    if well_midponts is None:
+        well_midponts = geomodel_ijmatched.dropna(subset=uwi_col)
+    df_out_index = geomodel_ijmatched[uwi_col].dropna().unique()
+    df_out_columns = geomodel_ijmatched.iloc[0:1].agg(agg_arg).columns
+    well_properties = pd.DataFrame(index=df_out_index, columns=df_out_columns)
+
+    for _, (i, j, z, uwi) in well_midponts[
         ["i_index", "j_index", "z_coord", uwi_col]
     ].iterrows():
-        ijmatch = df_ijmatched.xs((i, j), level=(0, 1), drop_level=False)
+        ijmatch = geomodel_ijmatched.xs((i, j), level=(0, 1), drop_level=False)
         zmatch = ijmatch[abs(ijmatch["z_coord"] - z) < zdmax]
-        agged = zmatch.agg(agg_arg).T.squeeze()
-        df_out.loc[uwi] = zmatch.agg(agg_arg).iloc[0]
-    return df_out
+        well_properties.loc[uwi] = zmatch.agg(agg_arg).iloc[0]
+    return well_properties
 
 
 def get_facies_stats(df, zonename="Mainzones", faciesname="Facies", attrs=None):
@@ -202,36 +249,53 @@ def get_facies_stats(df, zonename="Mainzones", faciesname="Facies", attrs=None):
 
 
 def get_facies_histograms(
-    df,
-    zone_name="Mainzones",
-    facies_name="Facies",
-    properties=["Phi", "Sw"],
-    ooip_name="OOIP",
-    print_progress=True,
-):
-    zones = df[zone_name].unique().compute()
-    facies = df[facies_name].unique().compute()
+    geomodel: dd.DataFrame,
+    zone_name: str = "Mainzones",
+    facies_name: str = "Facies",
+    properties: list[str] | None = None,
+    ooip_name: str = "OOIP",
+) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """Calculate histogram of original oil in place for geomodel by zone, facies, properties.
+
+    Args:
+        geomodel (dd.DataFrame): geocellular model with zone, facies, properties
+        zone_name (str, optional): column naming the zone. Defaults to "Mainzones".
+        facies_name (str, optional): column naming the facies. Defaults to "Facies".
+        properties (list[str], optional): columns holding properties to compute histogram over.
+            Defaults to ["Phi", "Sw"].
+
+        ooip_name (str, optional): Property to calculate the histogram over. Defaults to "OOIP".
+
+    Returns:
+        tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]: ooip_splits, index_conversion, and
+            max property values
+    """
+    if properties is None:
+        properties = ["Phi", "Sw"]
+    zones = geomodel[zone_name].unique().compute()
+    facies = geomodel[facies_name].unique().compute()
 
     ooip_splits = pd.DataFrame(
         columns=pd.MultiIndex.from_product(
-            [zones.values, facies.values, properties],
+            [zones.to_numpy(), facies.to_numpy(), properties],
             names=["Zone", "Facies", "Property"],
         )
     ).sort_index(level="columns")
 
     # percentage of max for each property done over zone, facies
-    prop_max = df[properties].max().compute()
+    prop_max = geomodel[properties].max().compute()
     for z in zones:
         for face in facies:
-            df_slice = df[(df[zone_name] == z) & (df[facies_name] == face)]
+            df_slice = geomodel[
+                (geomodel[zone_name] == z) & (geomodel[facies_name] == face)
+            ]
             for p in properties:
                 df_slice[p + "_"] = (
                     (df_slice[p] // (prop_max[p] / 100.0)).dropna().astype(int)
                 )
                 split = df_slice.groupby(p + "_")[ooip_name].sum().compute()
                 ooip_splits.loc[:, (z, face, p)] = split
-                if print_progress:
-                    print("finished zone", z, "facies", face, "property", p)
+                logging.info("finished zone", z, "facies", face, "property", p)
 
     ooip_splits = ooip_splits.sort_index()
     # provide translation from index to x values for histogram
